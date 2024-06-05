0f6d4ea8502013ac12164acd0c0b38bd879bada2
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2024-06-05 11:33:17 +0900

    포트폴리오 header부분 작업

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">안녕하세요! 김내우입니다!</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section--title--color">Frontend</span> <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit, amet consectetur adiposicing elit.
+            <br />
+            Dolorum, quas. Amet soluta assumenda cum?
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+        <div className="hero--section--img">
+          <img src="./img/hero_img.jpg" alt="Hero Section" />
+        </div>
+      </div>
+    </section>
+  );
+}
