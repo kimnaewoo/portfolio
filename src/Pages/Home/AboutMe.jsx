07aa871f026cad265ab2142702cf466257fc185a
kimnaewoo <140692045+kimnaewoo@users.noreply.github.com>
2024-06-28 14:52:38 +0900

    포트폴리오 자기소개 수정

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -1,3 +1,7 @@
+import { GoPersonFill } from 'react-icons/go';
+import { IoIosMail } from 'react-icons/io';
+import { FaBirthdayCake, FaHome } from 'react-icons/fa';
+
 export default function AboutMe() {
   return (
     <section id="AboutMe" className="about--section">
@@ -9,21 +13,24 @@ export default function AboutMe() {
           <p className="section--title">About</p>
           <h1 className="skills--section--heading">About Me</h1>
           <p className="hero--section--description">
-            이름 : 김내우
-            <br />
-            나이 : 1994. 07. 15
-            <br />
-            주소 : 서울시 관악구 <br />
-            메일 : gnag0403@naver.com <br />
-            {/* 안녕하세요!
-            <br />
-            다양한 배경으로 성장에 노하우가 있는 프론트엔드 신입개발자 김내우입니다. 최신 기술과 이슈를 끊임없이
-            연구하고 꾸준히 학문을 쌓아, 작은 부분까지도 가장 효율적인 방법으로 구현할수 있는 개발자가 되는것이
-            목표입니다. <br />
-            그렇기에, 프론트엔드 개발자를 하기 위해 학원을 다니며 실무에 필요한 지식을 습득했고 지금은 취업을 위해
-            포트폴리오를 만들며 부족한 부분을 채워나가고 있습니다. <br />
-            빠르게 변하는 웹개발트랜드 속에서, 다양한 경험으로 배경을 갖춘 저에게 비전공은 더 큰 전공이라고 생각하고,
-            새로운 시도와 함께 사용자 입장을 고려하며 성실하게 성장하는 프론트엔드 개발자가 되겠습니다. */}
+            <ul>
+              <li className="hero--section--introduce">
+                <GoPersonFill />
+                <span>이름 : 김내우</span>
+              </li>
+              <li className="hero--section--introduce">
+                <FaBirthdayCake />
+                <span>나이 : 1994. 07. 15</span>
+              </li>
+              <li className="hero--section--introduce">
+                <FaHome />
+                <span>주소 : 서울시 관악구</span>
+              </li>
+              <li className="hero--section--introduce">
+                <IoIosMail />
+                <span>메일 : gnag0403@naver.com</span>
+              </li>
+            </ul>
           </p>
         </div>
       </div>
